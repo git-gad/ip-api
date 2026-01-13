8681ef77aeaa558744357a1f6e7bd2ceb26d2c64
gad <gadvineblat@gmail.com>
2026-01-13 11:58:52 +0200

    service-a working localy

--- service-a/app/services.py
+++ service-a/app/services.py
@@ -4,6 +4,7 @@ URL = 'http://ip-api.com/json/'
 FIELDS = 'lat,lon'
 
 def get_ip_coordinates(ip):
-    response = requests.get(f'{URL}{ip}?fields={FIELDS}')
-    print(response)
-    return response
+    response = requests.get(f'{URL}{ip.ip}?fields={FIELDS}')
+    return {'ip': ip.ip,
+        'coordinates': response.text
+    }
